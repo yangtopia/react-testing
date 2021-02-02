be2cfb1925e98c4675134ef49e696f16db5ee2a0
yangtopia <yangtopia@gmail.com>
2021-02-02 23:22:41 +0900

    fix: change text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          리액트를 배워보자!
         </a>
       </header>
     </div>
